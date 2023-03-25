5305a23da9b9287e8dfa8392b63cd891208e9746
titto <tit.tolk07022003@gmail.com>
2023-03-25 18:28:38 +0700

    Верстка 2 экрана без карточек

---
+++ app/src/main/java/com/example/lesson22032023/TopicActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lesson22032023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TopicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_topic)
+    }
+}
