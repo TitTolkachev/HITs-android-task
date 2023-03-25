24c41ea736cf3234d559f850110d92784ae55f2c
titto <tit.tolk07022003@gmail.com>
2023-03-25 18:50:12 +0700

    Переход между экранами Изменены цвета темы

--- app/src/main/java/com/example/lesson22032023/SignUpActivity.kt
+++ app/src/main/java/com/example/lesson22032023/SignUpActivity.kt
@@ -1,11 +1,30 @@
 package com.example.lesson22032023
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lesson22032023.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonSignUp.setOnClickListener {
+            val intent = Intent(this, TopicActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        overridePendingTransition(0, 0)
     }
 }
